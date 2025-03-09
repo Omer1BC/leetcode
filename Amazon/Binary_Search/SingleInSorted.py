0b86eecd5dd3600cb7fb90027bff0381ccc3efe3
omer18 <omer18@vt.edu>
2025-03-09 14:47:16 -0400

    more mock exmaples

---
+++ Amazon/Binary_Search/SingleInSorted.py
@@ -0,0 +1,19 @@
+'''
+You are given a sorted array consisting of only integers where every element appears 
+exactly twice, except for one element which appears exactly once.
+
+Return the single element that appears only once.
+'''
+
+'''
+preconditions: 
+    -nums can not be null
+    -len(nums) > 0
+constraints:
+    -1 element with a freq of 1 else elements have a freq of 2
+    -if 1 element found return it
+'''
+def solve(nums:List[int]) -> int:
+
+
+
